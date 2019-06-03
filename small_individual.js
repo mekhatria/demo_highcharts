d20804568c958fe2905ffafb3cd824faa3943df0
mustapha mekhatria <mustapha@highsoft.com>
2019-06-03 14:37:04 +0200

    Update the r of the circle

--- small_individual.js
+++ small_individual.js
@@ -3,7 +3,7 @@
     Highcharts.SVGRenderer.prototype.symbols.individual = function(x, y, w, h, r) {
         return [
             'M', x+w*(2/5), y+h/2, 
-            'A', 5, 5, 0,1,1, x+(w*3/5),y+h/2 ,
+            'A', w/3, w/3, 0,1,1, x+(w*3/5),y+h/2 ,
             'L', x+(w*4/5), y+h,
             x+(w*1/5),y+h
         ];
